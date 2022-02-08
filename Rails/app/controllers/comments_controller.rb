acc19df2cefd8b1f36c623de0ca987c1d6beab61
skonishi1125 <skonishi1125@gmail.com>
2022-02-08 20:48:14 +0900

    commentsモデルのedit,updateを実装.

--- Rails/app/controllers/comments_controller.rb
+++ Rails/app/controllers/comments_controller.rb
@@ -13,6 +13,30 @@ class CommentsController < ApplicationController
 
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    is_update = @comment.update(comment_params)
+    session[:test_session] = 'セッションデータです'
+    cookies[:test_cookie] = 'クッキーだよ'
+    if is_update == true
+      @board = Board.find(@comment.board_id)
+      session[:test_session] = nil # session削除
+      cookies.delete :test_cookie # cookieの削除
+      redirect_to @board
+    else
+      redirect_to :back, flash: {
+        comment: @comment,
+        error_messages: @comment.errors.full_messages,
+        flash_name: 'flash_nameです',
+        flash_param: params
+      }
+    end
+  end
+
   def destroy
     # board = Board.find(params[:id])
     comment = Comment.find(params[:id])
--- Rails/config/routes.rb
+++ Rails/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index' #ヘッダリンク用 http://localhost:3000/
   resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :comments, only: %i[create destroy]
+  resources :comments, only: %i[create destroy edit update]
 end
