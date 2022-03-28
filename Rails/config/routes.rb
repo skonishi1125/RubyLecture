f910adbdc4922346e99984c41343225279103702
skonishi1125 <skonishi1125@gmail.com>
2022-03-28 22:11:16 +0900

    part95までやった

--- Rails/config/routes.rb
+++ Rails/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/me'
+
+  get 'home/index'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'home#index' #ヘッダリンク用 http://localhost:3000/
   resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :comments, only: %i[create destroy edit update]
